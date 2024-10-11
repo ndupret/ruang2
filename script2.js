bd938683bcfe9ca411424f3fe297455a1749aea4
ndupret <152419565+ndupret@users.noreply.github.com>
2024-10-11 20:20:14 +0700

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,9 @@
+document.getElementById('wish-button').addEventListener('click', function() {
+    // Redirect ke website yang dituju
+    window.location.href = 'index3.html'; // Ganti dengan URL tujuan
+});
+
+window.addEventListener('load', function() {
+    const music = document.getElementById('birthday-song');
+    music.play();
+});
